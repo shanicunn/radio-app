1d7a216359d4351a677ca13895f7c7eb18d0e276
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-07 22:29:37 -0400

    Replaced nonworking songs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,10 +18,10 @@ let stations = [{
     {
         name: "R&B",
         songs: [{
-                title: "Slow Wine Bass Line",
-                artist: "Lloyd",
-                image: "assets/Lloyd-Ft.-Teddy-Riley-E28093-Slow-Wine-Bass-Line-Mp3-Audio-Download-300x300.jpg",
-                sound: "assets/audio/Lloyd-Slow-Wine-Bass-Line-ft-Teddy-Riley.mp3.mp3",
+                title: "Be Honest",
+                artist: "Jorja Smith ft Burna Boy",
+                image: "assets/jorjasmith-be-honest.jpg",
+                sound: "assets/audio/Jorja_Smith_-_Be_Honest_ft_Burna_Boy_talkglitz.tv.mp3",
             },
             {
                 title: "Bussit",
@@ -35,10 +35,10 @@ let stations = [{
     {
         name: "Hip-Hop",
         songs: [{
-                title: "Need Me",
-                artist: "JI the Prince of NY",
-                image: "assets/J.I-the-Prince-of-N.Y-Hood-Life-Krisis-Vol.-1-cover-art.jpg",
-                sound: "",
+                title: "Yea Yea",
+                artist: "Pop Smoke",
+                image: "assets/Pop-Smoke-Shoot-for-the-Stars-Aim-for-the-Moon-Deluxe-Album-cover.jpg",
+                sound: "assets/audio/06-Yea-Yea.mp3",
             },
             {
 
@@ -56,24 +56,24 @@ let stations = [{
 let changeShuffle = () => {
     let stationChange = Math.floor(Math.random() * 3);
     // console.log(stationChange);
-    let stationPlaying = stations[stationChange];
+    let stationPlaying = stations[stationChange].name;
     let songChange = Math.floor(Math.random() * 2);
     // console.log(songChange);
     let songPlaying = stations[stationChange].songs[songChange].title;
     let songArtist = stations[stationChange].songs[songChange].artist;
     let musicUrl = stations[stationChange].songs[songChange].sound;
+    let songCover = stations[stationChange].songs[songChange].image;
     
     // console.log(songPlaying);
     console.log("Now Playing:", songPlaying, "by", songArtist);
 
-    document.getElementById("station").innerHTML ="Station:  " + stations[stationChange].name;
-    document.getElementById("artist").innerHTML = "Artist: " + stations[stationChange].songs[songChange].artist;
-    document.getElementById("song").innerHTML = "Song: " + stations[stationChange].songs[songChange].title;
-    document.getElementById("img").src = stations[stationChange].songs[songChange].image;
-    document.getElementById("music").setAttribute('src', stations[stationChange].songs[songChange].sound);
+    document.getElementById("station").innerHTML ="Station:  " + stationPlaying;
+    document.getElementById("artist").innerHTML = "Artist: " + songArtist;
+    document.getElementById("song").innerHTML = "Song: " + songPlaying;
+    document.getElementById("img").src = songCover;
+    document.getElementById("music").setAttribute('src', musicUrl);
     document.getElementById("music").load();
     document.getElementById("music").play();
-    // document.getElementById("music").src= stations[stationChange].songs[songChange].music;
     return stationPlaying, songPlaying;
 };
 
